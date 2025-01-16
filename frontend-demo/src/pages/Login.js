cee743f408ad23220a2c0647fb65c54078d7e02a
nileshwarya r roonwal <nrroonwal@gmail.com>
2025-01-16 20:39:31 +0530

    login-page

---
+++ frontend-demo/src/pages/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Login.css'; // Importing CSS file
+
+const Login = () => {
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h2>Login</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Enter your username" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
